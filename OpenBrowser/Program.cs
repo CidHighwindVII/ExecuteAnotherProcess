a583aad38f1632cdb55a5df4950da432060efb11
Carlos Leite <CarlosLeite@criticalmanufacturing.com>
2021-10-22 18:52:27 +0100

    .

---
+++ OpenBrowser/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OpenBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = string.Empty;
+            Thread t = new Thread((ThreadStart)(() =>
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.Filter = "Todos os ficheiros|*.*";
+                    openFileDialog.FilterIndex = 1;
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        filePath = openFileDialog.FileName;
+                    }
+                }
+            }));
+
+            // Run your code from a thread that joins the STA Thread
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
+
+            if (File.Exists("Teste.txt"))
+            {
+                File.Delete("Teste.txt");
+            }
+
+            File.AppendAllText("Teste.txt", $"Olha que bonito, escolheste um ficheiro em {filePath}");
+        }
+    }
+}
---
+++ Workbench/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Workbench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = "OpenBrowser.exe";
+            process.Start();
+            process.WaitForExit();
+
+            if (File.Exists("Teste.txt"))
+            {
+                var text = File.ReadAllText("Teste.txt");
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine("Nada encontrado");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
